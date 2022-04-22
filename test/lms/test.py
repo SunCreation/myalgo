bb246b62c7fdffaf8aa2afc1bdd3635049b0db26
SunCreation <cake0103@nate.com>
2022-04-22 11:36:58 +0900

    lms_fd8

--- test/lms/test.py
+++ test/lms/test.py
@@ -1,22 +1,8 @@
-from hi.python.learn import hello, Class_
+print('test모듈이 잘 임포트 되었습니다.')
 
-hello()
+def f():
+    ...
 
-x = Class_()
-
-print(x.data)
-
-
-
-print('%03d' %int('23'))
-a = '97.xlsx 98.docx 99.docx 100.xlsx 101.docx 102.docx'
-files = a.split()
-
-
-
-
-print([(lambda x: '%03d' %int(x[0]))(x:=i.split('.')) + '.'+ x[1] for i in files])
-
-
-
-print([(lambda x: '%03d' %int(x[0]))(i.split('.')) + '.'+ i.split('.')[1] for i in files])
+print(__name__)
+if __name__=='__main__':
+    print('이 명령은 실행 했을 때만 해라.')
---
+++ test/lms/test2.py
@@ -0,0 +1,4 @@
+import test
+import stats
+
+print(stats.means([1,2,3,4,5]))
