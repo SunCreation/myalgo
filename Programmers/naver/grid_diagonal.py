4ea832be34f7f305ade1b2f4fece60bcf01158e6
{SunCreation} <{cake0103@nate.com}>
2022-07-11 04:22:58 +0900

    coding_test: naver ai rush

--- Programmers/naver/grid_diagonal.py
+++ Programmers/naver/grid_diagonal.py
@@ -1,11 +1,61 @@
-# 
+# 아래와 같이 있다고 생각해보자.
+# 1 0 0 1
+# 2 0 0 2
+# 1 1 1 0
+# 0 2 0 1
+# (a,b)좌표가 들어올 시 (a-1,b-1), (a-1,b), (a,b-1), (a,b) 네 위치를 확인하면 된다. 물론 범위 안에서만
+#  확인했을 시 결과 -> (a-1,b-1),(a-1,b+1),(a+1,b-1),(a+1,b+1)
+# 대각선을 확인해가며 좌표를 checklist에 모두 담는다.
+# 이후 점들에서 겹치는게 있으면 fail! 매우 좋은 방법이라고 생각된다.
+
+from itertools import permutations
+from copy import deepcopy
 
 def solution(grid):
     answer = []
+    checklist = set()
+    N = len(grid)
+
+    def check_all(posis) -> bool:
+        checklist.clear()
+        for pos in zip(range(5),posis):
+            if not check(pos): return False
+        return True
+
+    def check(now):
+        task = [now]
+        grid_ = deepcopy(grid)
+
+        while task:
+            x,y = task.pop()
+            if (x,y) in checklist: return False
+            checklist.add((x,y))
+            
+            if x-1 >= 0 and y-1 >= 0 and grid_[x-1][y-1]==0:
+                task.append((x-1,y-1))
+                grid_[x-1][y-1] = 2
+            if x < N and y-1 >= 0 and grid_[x][y-1]==1:
+                task.append((x+1,y-1))
+                grid_[x][y-1] = 2
+            if x-1 >= 0 and y < N and grid_[x-1][y]==1:
+                task.append((x-1,y+1))
+                grid_[x-1][y] = 2
+            if x < N and y < N and grid_[x][y]==0:
+                task.append((x+1,y+1))
+                grid_[x][y] = 2
+            print(task)
+        return True
+
+    for i in permutations(range(N+1)):
+        if check_all(i):
+            answer.append(i)
+    print(answer, len(answer))
+    return answer
+    return [[0,1,2,3,4]]
     return answer
 
 if __name__ == '__main__':
-    import sys
-    sys.stdin = open('data')
-    grid = eval(input())
+    import os
+    os.system("python3 make_grid.py")
+
     
--- Programmers/naver/make_grid.py
+++ Programmers/naver/make_grid.py
@@ -4,53 +4,60 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 import sys
+from time import sleep, time
+
+from pyparsing import alphas
 from grid_diagonal import solution
-sys.stdin = open("data")
 
 def fig2array(fig):
     fig.canvas.draw()
     return np.array(fig.canvas.renderer._renderer)
 
-def point(draw, pos=None, size=5, color=(210,20,250)):
-    if pos is None:
-        raise ValueError("Experted argument 'pos' is not given")
-    x,y = pos
-    draw.ellipse((x_ticks[x]-size,y_ticks[y]-size,x_ticks[x]+size,y_ticks[y]+size),fill=color)
+def point(draw, *posis, size=5, color=(210,20,250)):
+    if posis is None:
+        raise ValueError("Experted argument 'posis' is not given")
+    for pos in posis:
+        # 점 찍어주는 것은 x,y 좌표가 생각과 다르기 때문에, 순서를 뒤집는다.
+        y,x = pos
+        draw.ellipse((x_ticks[x]-size,y_ticks[y]-size,x_ticks[x]+size,y_ticks[y]+size),fill=color )
 
 
 if __name__ == '__main__':
+    sys.stdin = open("data")
+    grid = eval(sys.stdin.readline())
+    print(grid)
     height = 600
     width = 600
+    N = len(grid)
     image = Image.new(mode='P', size=(height, width), color=255)
     # Draw some lines
     draw = ImageDraw.Draw(image)
+    # plt.imshow(image)
     offset = 20
     y_start = offset
     y_end = image.height - offset
     x_start = offset
     x_end = image.width - offset
-    step_size = int((x_end - offset) / 4)
+    step_size = int((x_end - offset) / N)
     x_ticks = []
     y_ticks = []
 
     for x in range(offset, x_end+1, step_size):
         x_ticks.append(x)
         line = ((x, y_start), (x, y_end))
-        draw.line(line, fill=28)
+        draw.line(line, fill=(28,28,28))
     for y in range(offset,y_end+1, step_size):
         y_ticks.append(y)
         line = ((x_start, y), (x_end, y))
-        draw.line(line, fill=28)
-    grid = eval(sys.stdin.read())
-    print(grid)
+        draw.line(line, fill=(28,28,28))
     for j, line in enumerate(grid):
         for i, pos in enumerate(line):
             if pos==0:
                 line = ((x_ticks[i],y_ticks[j]),(x_ticks[i+1],y_ticks[j+1]))
-                draw.line(line, fill=28)
+                draw.line(line, fill=(28,28,28))
             if pos==1:
                 line = ((x_ticks[i],y_ticks[j+1]),(x_ticks[i+1],y_ticks[j]))
-                draw.line(line, fill=28)
+                draw.line(line, fill=(28,28,28))
     # 예시
     # point(draw, (0,4))
     # point(draw, (1,3))
@@ -58,7 +65,7 @@ if __name__ == '__main__':
     # point(draw, (3,1))
     # point(draw, (4,0))
 
-    del draw
+    # del draw
     f = plt.figure()
     plt.imshow(image)
     plt.axis('off')
@@ -66,10 +73,39 @@ if __name__ == '__main__':
 
     # print(fig2array((f)))
     hi = fig2array((f))#np.asarray(image)
+
     while True:
-        cv2.imshow("test",hi)
-        if cv2.waitKey(1) & 0xff == ord('w'):
-            break
+        for posis in (list(zip(range(5), i)) for i in solution(grid)):
+            st = time()
+            while True:
+                pallets = [image.copy() for i in range(3)]
+                draws = [ImageDraw.Draw(pallet) for pallet in pallets]
+                t = time() - st
+                point(draws[2], *posis, color=255+int(160*t*(t-2)))
+                point(draws[1], *posis, color=255+int(160*t*(t-2)))
+                # point(draws[2], *posis, color=255+int(150*t*(t-2)))
+
+                # f = plt.figure()
+                # plt.imshow(pallet)
+                # plt.axis('off')
+                # pallet = fig2array(f)
+                # print(pallet)
+                # print(np.array(pallet).shape)
+
+                c_pallet = np.stack([np.array(pallet) for pallet in pallets], axis=-1)
+                cv2.imshow("test",c_pallet)
+                # pallet = image.copy()
+                # sleep(10)
+                # f = plt.figure()
+                # plt.imshow(pallet)
+                # plt.axis('off')
+                # pallet = fig2array(f)
+                # cv2.imshow("test",pallet)
+                if cv2.waitKey(1) & 0xff == ord('w') or t > 2:
+                    break
+
+        sleep(3)
+        break
     cv2.destroyAllWindows()
     # print(hi)
     # plt.show()
